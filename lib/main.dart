11a5a385867d5ec1cc8ee61081167643fe032328
Mohamed <mohamed2852002@gmail.com>
2024-11-26 21:36:32 +0200

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,21 @@
+import 'package:bookly_app/core/themes/app_theme.dart';
+import 'package:bookly_app/features/splash/presentation/views/splash_view.dart';
+import 'package:flutter/material.dart';
+import 'package:get/get_navigation/src/root/get_material_app.dart';
+
+void main() {
+  runApp(const Bookly());
+}
+
+class Bookly extends StatelessWidget {
+  const Bookly({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return GetMaterialApp(
+      theme: AppTheme.appTheme,
+      debugShowCheckedModeBanner: false,
+      home: const SplashView(),
+    );
+  }
+}
