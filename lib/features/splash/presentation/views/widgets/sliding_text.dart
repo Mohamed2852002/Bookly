b53f505d5f51c47447da7d7fe20e0cd01b74c668
Mohamed <mohamed2852002@gmail.com>
2024-11-27 16:33:31 +0200

    refactor and add navigation to home

--- lib/features/splash/presentation/views/widgets/sliding_text.dart
+++ lib/features/splash/presentation/views/widgets/sliding_text.dart
@@ -1,5 +1,7 @@
+import 'package:bookly_app/features/home/presentation/views/home_view.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:get/get.dart';
 
 class SlidingText extends StatefulWidget {
   const SlidingText({super.key});
@@ -12,16 +14,18 @@ class _SlidingTextState extends State<SlidingText>
     with SingleTickerProviderStateMixin {
   late AnimationController animationController;
   late Animation<Offset> slidingAnimation;
+
   @override
   void initState() {
-    animationController =
-        AnimationController(vsync: this, duration: const Duration(seconds: 1));
     super.initState();
-    slidingAnimation = Tween<Offset>(
-      begin: const Offset(0, 10),
-      end: const Offset(0, 0),
-    ).animate(animationController);
-    animationController.forward();
+    initSlidingAnimation();
+    navigateToHome();
+  }
+
+  @override
+  void dispose() {
+    super.dispose();
+    animationController.dispose();
   }
 
   @override
@@ -39,6 +43,26 @@ class _SlidingTextState extends State<SlidingText>
           ),
         ),
       ),
+    );
+  }
+
+  void initSlidingAnimation() {
+    animationController =
+        AnimationController(vsync: this, duration: const Duration(seconds: 1));
+
+    slidingAnimation = Tween<Offset>(
+      begin: const Offset(0, 10),
+      end: const Offset(0, 0),
+    ).animate(animationController);
+    animationController.forward();
+  }
+
+  void navigateToHome() {
+    Future.delayed(
+      const Duration(seconds: 2),
+      () {
+        Get.to(const HomeView(), transition: Transition.leftToRight);
+      },
     );
   }
 }
